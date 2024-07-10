5cc73bea6aba2994ef89c8df77efe64aa1b6dd0b
qw <abow777@protonmail.com>
2024-07-09 23:05:59 -0400

    update script for downloading from gdrive

--- gd.py
+++ gd.py
@@ -1,27 +1,40 @@
-# Description: Download a file from Google Drive 
-
-# Usage: python gd.py <URL> --output <output_dir>
-
 import argparse
 import re
 import gdown
+import os
 
-def extract_file_id(url):
-    # Use a regular expression to extract the file ID from the URL
-    match = re.search(r'/file/d/([a-zA-Z0-9_-]+)', url)
-    if match:
-        return match.group(1)
+def extract_id(url):
+    # Use a regular expression to extract the file or folder ID from the URL
+    file_match = re.search(r'/file/d/([a-zA-Z0-9_-]+)', url)
+    folder_match = re.search(r'/folders/([a-zA-Z0-9_-]+)', url)
+    
+    if file_match:
+        return file_match.group(1), 'file'
+    elif folder_match:
+        return folder_match.group(1), 'folder'
     else:
         raise ValueError("Invalid Google Drive URL")
 
+def create_directory_if_not_exists(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+        print(f"Created directory: {path}")
+    else:
+        print(f"Directory already exists: {path}")
+
 def download_from_google_drive(url, output_path):
-    file_id = extract_file_id(url)
-    gdown.download(f"https://drive.google.com/uc?id={file_id}", output_path, quiet=False)
+    item_id, item_type = extract_id(url)
+    create_directory_if_not_exists(os.path.dirname(output_path))
+    
+    if item_type == 'file':
+        gdown.download(f"https://drive.google.com/uc?id={item_id}", output_path, quiet=False)
+    elif item_type == 'folder':
+        gdown.download_folder(f"https://drive.google.com/drive/folders/{item_id}", output=output_path, quiet=False)
 
 def main():
-    parser = argparse.ArgumentParser(description="Download a file from Google Drive and specify the target directory.")
+    parser = argparse.ArgumentParser(description="Download a file or folder from Google Drive and specify the target directory.")
     parser.add_argument("url", help="Google Drive shared URL")
-    parser.add_argument("--output", default='./', help="Path to the target directory and file name")
+    parser.add_argument("output", default='/data/ds', help="Path to the target directory and file name or folder")
 
     args = parser.parse_args()
 
@@ -29,3 +42,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
